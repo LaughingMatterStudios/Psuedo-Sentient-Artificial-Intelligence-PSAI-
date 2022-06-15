b16aa464f6fda02e2389be84bc0fa5bce6d6a939
TaylorWayneWilson <64389875+TaylorWayneWilson@users.noreply.github.com>
2022-06-15 09:36:32 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+Install each of the libraries below. Alternatively, run this file with "./" from a terminal.
+
+pip install pyttsx3
+pip install SpeechRecognition
+pip install webbrowser
+pip install wikipedia
