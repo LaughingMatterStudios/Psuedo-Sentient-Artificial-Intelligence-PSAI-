8279d43145a0e2fc19dc1881ba485b2b86c1c40c
TaylorWayneWilson <64389875+TaylorWayneWilson@users.noreply.github.com>
2022-06-15 14:23:02 -0500

    Create Servos.py

---
+++ Servos.py
@@ -0,0 +1,5 @@
+#If your bot has servos, define them here.
+
+#In the case of mine, I have 9 servos. I'm building this file out for my bot mainly. It uses an Arduino Pro Mini, so I'm starting with that.
+
+
